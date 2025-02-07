01649a7dc667a17bba2f9df1bec8c87d0699df94
acxanime <acxanime07@gmail.com>
2025-02-07 16:26:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,13 +4,13 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", 28918271))
     API_HASH = os.environ.get("API_HASH", "29bf447b916a795191046a91317869fb")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7707522221:AAFEc5JiXCPydeA3bTN77a8L1M0IwtMgd-M")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "LinkSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQFqAQUAILF71ce55v3V4Q_RX28TKyzEz2D-B5HD0T6Q4mAuiNS1GKshZJUSfzKmGw00CqAuPQUxbOn_gxDC0qjyHTcuo30gVOuTbWL3AWnd6Z5Sef4lP_AhSxMhUuLjDNaGUHpZ0s3PBh6Z9cMXBmLMHbOj9jRHd9Tu8PfMHmTSJQ-t_uLJQSlNMmOORHD13tvMFWhBsO5NJqTvB00DclFyYRxZt5Y8L9oFajnDUBpgEAe2Q5JmUJtdUcNPsMfXa8FQ6fKlYSwXpHsCEUXoyDLiZTjcZb0DOl3Pa1uFuTTa1_R0bNSSQFB-hEcmoeuazY7YPXMelePqOulfAgGra_T3V1n-yQAAAAF24wVEAA")
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002471954376"))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME","Misss_Lucy_bot")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER","6692613520"))
-    DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://sitaratoons:sitaratoons@cluster0.98nq3.mongodb.net/?retryWrites=true&w=majority")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Misss_Lucy_bot")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "6692613520"))
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://sitaratoons:sitaratoons@cluster0.98nq3.mongodb.net/?retryWrites=true&w=majority")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Link Search Bot.
     

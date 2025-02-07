26cd5a6e03246cad089b7d11b593ff6c2777108a
acxanime <acxanime07@gmail.com>
2025-02-07 16:18:23 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,10 +7,10 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "LinkSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME","")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER",""))
+    DATABASE_URL = os.environ.get("DATABASE_URL","")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Link Search Bot.
     

5808efb984d8d89798f8ffd2de67f78fa707e71b
acxanime <acxanime07@gmail.com>
2025-02-07 17:30:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from bot import Bot
 
-print("Bot Started. Updates @Codeflix_Bots")
-Bot().run()
+print()
+print("-------------------- Initializing Telegram Bot --------------------")
+# Start Clients
+Bot.start()
+
+print("------------------------------------------------------------------")
+print()
+print(f"""
+ _____________________________________________   
+|                                             |  
+|          Deployed Successfully              |  
+|              Join @{Config.UPDATES_CHANNEL_USERNAME}                 |
+|_____________________________________________|
+    """)
+
+# User.start()
+with tbot, client:
+    tbot.run_until_disconnected()
+    client.run_until_disconnected()
+
+# Loop Clients till Disconnects
+idle()
+# After Disconnects,
+# Stop Clients
+print()
+print("------------------------ Stopped Services ------------------------")
+Bot.stop()
